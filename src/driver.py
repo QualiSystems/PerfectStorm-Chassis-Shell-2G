b79ead896ef93b4cc930f444466330c63519dba0
Yoram Shamir <yoram-s@qualisystems.com>
2017-11-27 15:16:15 +0200

    version 1.0.0

--- src/driver.py
+++ src/driver.py
@@ -1,9 +1,14 @@
+
+from cloudshell.shell.core.driver_context import AutoLoadAttribute
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
-from cloudshell.tg.breaking_point.helpers.context_utils import get_logger_with_thread_id, get_api
+from cloudshell.tg.breaking_point.helpers.context_utils import get_api
+
+from cloudshell.traffic import tg_helper
+
 from bp_chassis.runners.bp_autoload_runner import BPAutoloadRunner
 
 
-class BreakingPointChassisDriver(ResourceDriverInterface):
+class PerfectStormChassisDriver(ResourceDriverInterface):
     SUPPORTED_OS = 'Breaking Point'
 
     def __init__(self):
@@ -13,7 +18,8 @@ class BreakingPointChassisDriver(ResourceDriverInterface):
         """
         :type context: cloudshell.shell.core.driver_context.InitCommandContext
         """
-        pass
+        self.logger = tg_helper.get_logger(context)
+        self.autoload_runner = BPAutoloadRunner(context, self.logger, get_api(context), self.SUPPORTED_OS)
 
     def cleanup(self):
         pass
@@ -24,7 +30,40 @@ class BreakingPointChassisDriver(ResourceDriverInterface):
         :rtype: cloudshell.shell.core.driver_context.AutoLoadDetails
         """
 
-        logger = get_logger_with_thread_id(context)
-        api = get_api(context)
-        autoload_runner = BPAutoloadRunner(context, logger, api, self.SUPPORTED_OS)
-        return autoload_runner.discover()
+        auto_load_details = self.autoload_runner.discover()
+
+        address = context.resource.address
+        user = context.resource.attributes['User']
+        password = get_api(context).DecryptPassword(context.resource.attributes['Password']).Value
+        import paramiko
+        self.ssh = paramiko.SSHClient()
+        self.ssh.load_system_host_keys()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(hostname=address, username=user, password=password)
+        self.chan = self.ssh.invoke_shell()
+        self.ssh_call('bpsh')
+        self.ssh_call('set chassis [$bps getChassis]')
+        modules = {}
+        for resource in auto_load_details.resources:
+            if resource.model == 'Generic Traffic Generator Module':
+                relative_address = resource.relative_address
+                modules[relative_address] = self.ssh_call('$chassis getCardMode ' + relative_address[1:]).split()[0]
+        for resource in auto_load_details.resources:
+            if resource.model == 'Generic Traffic Generator Port':
+                port_module = resource.relative_address.split('/')[0]
+                if port_module in modules.keys():
+                    auto_load_details.attributes.append(AutoLoadAttribute(relative_address=resource.relative_address,
+                                                                          attribute_name='Supported Controller',
+                                                                          attribute_value=modules[port_module]))
+        return auto_load_details
+
+    def ssh_call(self, string, *args):
+        command = '{}\n'.format(string % args)
+        self.logger.debug('sending %s', command.rstrip())
+        command_len = self.chan.send(command)
+        resp = ''
+        while not resp.endswith('% '):
+            resp += self.chan.recv(9999)
+        ret_value = str(resp[command_len:-3].decode("utf-8").strip())
+        self.logger.debug('received %s', ret_value)
+        return ret_value
